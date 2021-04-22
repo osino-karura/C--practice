e2caaff12c28ae1a0bb0d7c4aed42798f2b58076
osino-karura <82496473+osino-karura@users.noreply.github.com>
2021-04-22 16:14:36 +0900

    4/22
    理解できるということは解けるということ。
    もう少し紙に書いたり固定観念を捨てて考えたら解けたから、答えを見るのを癖にしないようにする。

---
+++ A - Infinite Coins.cpp
@@ -0,0 +1,20 @@
+//発想できなかった。
+//もう少し考えてから答えを見るべき
+//泥臭く発想の固定観念を捨てて、問題を解くべきだった
+//とても悔しい
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n, a;
+  cin >> n >> a;
+  
+  if (n % 500 <= a) {
+    cout << "Yes" << endl;
+  }
+  else {
+    cout << "No" << endl;
+  }
+}
