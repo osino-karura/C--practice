8fcc31890601f0c641fff104f38789a50a686edb
osino-karura <82496473+osino-karura@users.noreply.github.com>
2021-04-29 00:33:39 +0900

    Add files via upload

---
+++ AtCoder practice/20210429/EX14 - 三人兄弟の身長差.cpp
@@ -0,0 +1,15 @@
+//https://atcoder.jp/contests/apg4b/tasks/APG4b_ci
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int A, B, C;
+  cin >> A >> B >> C;
+  
+  int maximum = max(max(A, B), C);
+  int minimum = min(min(A, B), C);
+  
+  cout << maximum - minimum << endl; 
+  
+}
